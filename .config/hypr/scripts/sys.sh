ce9d5d8d0a65c88d779df1233d0416cd9be9c12b
ナッティ <furytorch.1861@gmail.com>
2026-03-06 17:50:28 +0700

    Desktop file launcher

--- .config/hypr/scripts/sys.sh
+++ .config/hypr/scripts/sys.sh
@@ -4,7 +4,7 @@ pkill wmenu || true
 
 FLAG=$(cat "$HOME"/.config/wmenu)
 
-case "$(printf "kill\nkillall\nzzz\nreboot\nshutdown" | eval wmenu "${FLAG}" -l 5 -p "System")" in
+case "$(printf "kill\nkillall\nzzz\nreboot\nshutdown" | eval wmenu "${FLAG}" -p "System")" in
 kill)
   ps -u "$USER" -o pid,comm,%cpu,%mem |
     eval wmenu "${FLAG}" -p "Kill" |
